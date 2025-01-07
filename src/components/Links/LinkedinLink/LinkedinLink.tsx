53da952b391e26a09f3ddd3c62bf5010845b7fa6
Herring, Desmond M CW2 USARMY 10 MTN DIV 1 BCT (USA) <desmond.m.herring.mil@army.mil>
2025-01-07 11:31:25 -0600

    Added styling to links.

--- src/components/Links/LinkedinLink/LinkedinLink.tsx
+++ src/components/Links/LinkedinLink/LinkedinLink.tsx
@@ -2,6 +2,7 @@
 import { Container } from "react-bootstrap";
 import Image from 'react-bootstrap/Image';
 
+
 interface props {
     className: string
 }
@@ -9,7 +10,9 @@ interface props {
 const LinkedInLink = ({className}: props) => {
 
     return (
-        <Container className={`${className}`}>
+        <Container 
+            className={`${className}`}
+        >
             <Image src="src/assets/icons/linkedin-icon.png" roundedCircle />
         </Container>
     )
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 //Utilities
 import { Link } from "react-router-dom";
-import { Row, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
 
 //Components
@@ -9,6 +9,9 @@ import LinkedInLink from "../Links/LinkedinLink/LinkedinLink";
 import GithubLink from "../Links/GithubLink/GithubLink";
 import TwitterLink from "../Links/TwitterLink/TwitterLink";
 import ResumeLink from "../Links/ResumeLink/ResumeLink";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faUser, faScrewdriverWrench, faBriefcase, faDiagramProject } from '@fortawesome/free-solid-svg-icons';
+import { Tooltip } from 'react-tooltip';
 
 //CSS
 import './NavBar.css'
@@ -23,24 +26,53 @@ const NavBar = () => {
             </div>
             
             <Container className="navbar-link-container">
-                <Link to={'/'} className="navBar-link">Home</Link>
-                <Link to={'/about'} className="navBar-link">About</Link>
-                <Link to={'/skills'} className="navBar-link">Skills</Link>
-                <Link to={'/experience'} className="navBar-link">Experience</Link>
-                <Link to={'/projects'} className="navBar-link">Projects</Link>
+                <Link to={'/'} className="navBar-link">
+                    <FontAwesomeIcon icon={faHouse} className="navBar-icon"/>
+                    Home
+                </Link>
+                <Link to={'/about'} className="navBar-link">
+                    <FontAwesomeIcon icon={faUser} className="navBar-icon"/>
+                    About
+                </Link>
+                <Link to={'/skills'} className="navBar-link">
+                    <FontAwesomeIcon icon={faScrewdriverWrench} className="navBar-icon"/>
+                    Skills
+                </Link>
+                <Link to={'/experience'} className="navBar-link">
+                    <FontAwesomeIcon icon={faBriefcase} className="navBar-icon"/>
+                    Experience
+                </Link>
+                <Link to={'/projects'} className="navBar-link">
+                    <FontAwesomeIcon icon={faDiagramProject} className="navBar-icon"/>
+                    Projects
+                </Link>
                 <Link to={'/contact'} className="navBar-link">Contact</Link>
             </Container>
 
-            <Container className="navBar-icon-container">  
+            <div className="navBar-icon-container">  
                     <Col>
-                        <LinkedInLink className="nav-icon-link"/>
-                        <TwitterLink className="nav-icon-link"/>
+                        <LinkedInLink className="nav-icon-link linkedin"/>
+                        <Tooltip anchorSelect=".linkedin" place="top">
+                            My LinkedIn Profile
+                        </Tooltip>
+
+                        <TwitterLink className="nav-icon-link twitter"/>
+                        <Tooltip anchorSelect=".twitter" place="top">
+                            My X Profile
+                        </Tooltip>                        
                     </Col>
                     <Col>
-                        <GithubLink className="nav-icon-link"/>
-                        <ResumeLink className="nav-icon-link"/>
+                        <GithubLink className="nav-icon-link github"/>
+                        <Tooltip anchorSelect=".github" place="bottom">
+                            My Github Profile
+                        </Tooltip>
+
+                        <ResumeLink className="nav-icon-link resume"/>
+                        <Tooltip anchorSelect=".resume" place="bottom">
+                            View My Resume
+                        </Tooltip>
                     </Col>
-            </Container>
+            </div>
             
         </Container>
     )
