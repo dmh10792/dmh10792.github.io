9e617a5e63e56ef060c61aa010006fcf09f48901
Herring, Desmond M CW2 USARMY 10 MTN DIV 1 BCT (USA) <desmond.m.herring.mil@army.mil>
2025-04-16 08:46:42 -0500

    Adding text to skills page and changing size of skillboxes.

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,6 +1,7 @@
 
 import SkillBox from "../components/SkillBox/SkillBox";
 import { skills } from "../helpers/skills";
+import {Typography} from "@mui/material";
 
 const Skills = () => {
 
@@ -10,9 +11,8 @@ const Skills = () => {
 
     return (
             <div style={{
-                display: 'grid',
-                gridTemplateColumns: 'repeat(3, 1fr)',
-                gap: 0.5,
+                display: 'flex',
+                flexDirection: 'row',
                 maxHeight: '97%',
                 marginTop: '2%',
                 overflowY: 'auto',
@@ -21,7 +21,27 @@ const Skills = () => {
                 scrollbarWidth: 'none',
                 marginLeft: '3%',
             }}>
+                <div
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(3, 1fr)',
+                        gap: 0.25,
+                        flex: 1,
+                    }}
+                >
                     {skillComponents}
+                </div>
+
+                <div
+                    style={{
+                        flex: 1,
+                    }}
+                >
+                    <Typography>
+                        A short paragraph of text that explains how I am committed to continued learning.
+                        azlsljksdjlkcnskjdcns. skdjcnskjdncskjdncsd. sdkjcnskjdncksjdcksjdnc
+                    </Typography>
+                </div>
 
             </div>
     )
